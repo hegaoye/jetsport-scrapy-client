3b34d90789ac0d7b1661d3cb4c386b6f7f9b08fe
franz <jstyfranz@itcom888.com>
2020-03-19 12:04:07 +0800

    增加枚举类

--- src/dao/setting_dao.py
+++ src/dao/setting_dao.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-from src.base.databasetools import Sqlite3Tools
 from src.entity.setting import Setting
 
 
@@ -7,9 +6,6 @@ class SettingDao:
     """
     设置 dao层
     """
-
-    def __init__(self):
-        self.db = Sqlite3Tools()
 
     def load(self, key):
         """
@@ -27,21 +23,10 @@ class SettingDao:
         """
         return self.load(key).value
 
-    def update(self, key_enum, value):
-        """
-        更新设置值
-        :param key_enum:键枚举
-        :param value:值
-        """
-        sql = 'update setting set value="' + str(value) + '"  where key="' + key_enum.value + '"'
-        self.db.update(sql)
-
-    def insert(self, key_enum, value):
+    def insert(self, key, value):
         """
         插入一条设置
         :param key_enum: key枚举
         :param value: 值
         """
-        sql = "insert into setting(key,value) " \
-              "VALUES ('" + str(key_enum.value) + "','" + str(value) + "') "
-        self.db.insert(sql)
+        Setting.create(key=key, value=value)
