7ffdc44a38cc9bd2ea2d9f5c2c85b5cc2130d74d
franz <jstyfranz@itcom888.com>
2020-03-19 11:04:53 +0800

    增加枚举类

--- src/base/enum/task_pool_state_enum.py
+++ src/base/enum/task_pool_state_enum.py
@@ -1,11 +1,11 @@
+# coding=utf-8
 from enum import Enum
-
-"""
-可作业 Enable,作业中 Working,停止作业 Stop_Task,报错暂停 Stop_Error
-"""
 
 
 class TaskPoolStateEnum(Enum):
+    """
+    可作业 Enable,作业中 Working,停止作业 Stop_Task,报错暂停 Stop_Error
+    """
     Enable = "可作业"
     Working = "作业中"
     Stop_Task = "停止作业"
