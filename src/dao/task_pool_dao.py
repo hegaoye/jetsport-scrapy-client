f99275e0a6a38fd7d97ee0cf33c0e2201df6dbb7
franz <jstyfranz@itcom888.com>
2020-03-19 10:29:03 +0800

    add 依赖环境

--- src/dao/task_pool_dao.py
+++ src/dao/task_pool_dao.py
@@ -2,16 +2,18 @@
 
 from src.entity.task_pool import TaskPool
 
+"""
+任务池dao
+"""
+
 
 class TaskPoolDao:
 
     def insert(self, taskPool):
+        """
+         保存任务到任务池中
+        :param taskPool: 任务池数据
+        """
         TaskPool.create(code=taskPool.code, crawling_rule_code=taskPool.crawling_rule_code,
                         state=taskPool.state, ordinal=taskPool.ordinal, push_frequce=taskPool.push_frequce,
                         task_url=taskPool.task_url, header=taskPool.header)
-
-
-if __name__ == '__main__':
-    t = TaskPool()
-    t.code = "1111"
-    TaskPoolDao().insert(t)
