f91c79d60a565e3c5978903aafad0a354693f3ef
franz <jstyfranz@itcom888.com>
2020-03-20 11:25:20 +0800

    修改dao

--- src/dao/commands_dao.py
+++ src/dao/commands_dao.py
@@ -8,11 +8,25 @@ class CommandsDao:
     """
 
     def load(self, state) -> Commands:
-        return Commands.get(Commands.state == state)
+        """
+        查询一条指令
+        :param state: 状态
+        :return:  Commands
+        """
+        return Commands.select().where(Commands.state == state).order_by(Commands.order.asc())
 
     def insert(self, command) -> None:
+        """
+        插入一条指令
+        :param command: 指令
+        """
         Commands.create(code=command.code, command=command.command, type=command.type, order=command.order,
                         state=command.state)
 
-    def update(self, code, state):
+    def updateStateByCode(self, code, state) -> None:
+        """
+        根据code 更新指令状态
+        :param code: 编码
+        :param state: 状态
+        """
         Commands.update(state=state).where(Commands.code == code).execute()
