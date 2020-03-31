ede238e5a7eba707526cc6cd97f9fdce8e28b9ae
franz <jstyfranz@itcom888.com>
2020-03-31 15:22:32 +0800

    增加参数名设置

--- src/base/browser.py
+++ src/base/browser.py
@@ -11,9 +11,20 @@ class Browser:
 
     def __init__(self):
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        # 浏览器不提供可视化页面
+        chrome_options.add_argument('--headless')
+        # 谷歌文档提到需要加上这个属性来规避bug
         chrome_options.add_argument('--disable-gpu')
+        # 以最高权限运行
         chrome_options.add_argument('--no-sandbox')
+        # 启动就最大化
+        chrome_options.add_argument('--start-maximized')
+        # 以下用于有界面时配置项
+        # 隐藏滚动条, 应对一些特殊页面
+        # chrome_options.add_argument('--hide-scrollbars')
+        # 不加载图片, 提升速度
+        # chrome_options.add_argument('blink-settings=imagesEnabled=false')
+
         self.browser = webdriver.Chrome(executable_path=BROWSER_PATH, chrome_options=chrome_options)
         # self.browser.fullscreen_window()
 
