9a340bc32139567c1c8d1b56aa0828bd15771a33
franz <jstyfranz@itcom888.com>
2020-04-04 16:25:35 +0800

    修改构造参数的启动实例

--- src/thread/construct_data_thread.py
+++ src/thread/construct_data_thread.py
@@ -76,50 +76,48 @@ class ConstructDataThread(BaseTread, Singleton):
                 # 6.删除已经被构造的数据 todo 暂时注释避免数据被删，调试好后，需要解开注释
                 # self.crawlingRuleDataService.delete_list(id_list)
 
+    def __build_param(self, pre_id, parameter_data):
+        print(str(pre_id))
+        id_list = []
+        # 3.查询参数数据
+        crawling_rule_data_list = self.crawlingRuleDataService.list_by_pre_id(pre_id)
+        print(len(crawling_rule_data_list))
+        # 4.构造结构
+        for crawlingRuleData in crawling_rule_data_list:
+            if crawlingRuleData.pre_parameter_code:
+                parameter_load = self.parameterService.load(crawlingRuleData.pre_parameter_code)
+                if parameter_load:
+                    if ParameterTypeEnum.List.name.__eq__(parameter_load.parameter_type):
+                        pre_param_name = parameter_load.name
+                        sub_parameter_data_list = []
+                        sub_parameter_data = {}
+                        sub_parameter_data[crawlingRuleData.parameter_name] = crawlingRuleData.value
+                        sub_parameter_data_list.append(sub_parameter_data)
+                        if pre_param_name in parameter_data.keys():
+                            parameter_data[pre_param_name] = parameter_data[pre_param_name] + sub_parameter_data_list
+                        else:
+                            parameter_data[pre_param_name] = sub_parameter_data_list
+            else:
+                parameter_data[crawlingRuleData.parameter_name] = crawlingRuleData.value
+                sub_id_list, sub_parameter_data = self.__build_param(crawlingRuleData.code, parameter_data)
+                if id_list:
+                    id_list = id_list + sub_id_list
 
-def __build_param(self, pre_id, parameter_data):
-    print(str(pre_id))
-    id_list = []
-    # 3.查询参数数据
-    crawling_rule_data_list = self.crawlingRuleDataService.list_by_pre_id(pre_id)
-    print(len(crawling_rule_data_list))
-    # 4.构造结构
-    for crawlingRuleData in crawling_rule_data_list:
-        if crawlingRuleData.pre_parameter_code:
-            parameter_load = self.parameterService.load(crawlingRuleData.pre_parameter_code)
-            if parameter_load:
-                if ParameterTypeEnum.List.name.__eq__(parameter_load.parameter_type):
-                    pre_param_name = parameter_load.name
-                    sub_parameter_data_list = []
-                    sub_parameter_data = {}
-                    sub_parameter_data[crawlingRuleData.parameter_name] = crawlingRuleData.value
-                    sub_parameter_data_list.append(sub_parameter_data)
-                    if pre_param_name in parameter_data.keys():
-                        parameter_data[pre_param_name] = parameter_data[pre_param_name] + sub_parameter_data_list
-                    else:
-                        parameter_data[pre_param_name] = sub_parameter_data_list
-        else:
-            parameter_data[crawlingRuleData.parameter_name] = crawlingRuleData.value
-            sub_id_list, sub_parameter_data = self.__build_param(crawlingRuleData.code, parameter_data)
-            if id_list:
-                id_list = id_list + sub_id_list
+            id_list.append(crawlingRuleData.id)
 
-        id_list.append(crawlingRuleData.id)
+        return id_list, parameter_data
 
-    return id_list, parameter_data
-
-
-def __data_cache(self, api_code, api_parameter_list):
-    """
-    缓存 数据
-    :param api_code: 接口编码
-    :param api_parameter_list: 数据集合
-    :return:
-    """
-    dataCache = DataCache()
-    dataCache.api_code = api_code
-    dataCache.data = json.dumps(api_parameter_list)
-    self.dataCacheService.save(dataCache)
+    def __data_cache(self, api_code, api_parameter_list):
+        """
+        缓存 数据
+        :param api_code: 接口编码
+        :param api_parameter_list: 数据集合
+        :return:
+        """
+        dataCache = DataCache()
+        dataCache.api_code = api_code
+        dataCache.data = json.dumps(api_parameter_list)
+        self.dataCacheService.save(dataCache)
 
 
 if __name__ == '__main__':
